9061ecf19f9bd32a9262e847e66a9b525e204cd9
DCleve <david.cleveland@tcgplayer.com>
2025-02-21 02:45:53 +0000

    Feb 20, 2025, 9:45 PM

--- In Progress/Potential_Monetary_Loss_Due_to_Unprocessed_RIs.py
+++ In Progress/Potential_Monetary_Loss_Due_to_Unprocessed_RIs.py
@@ -18,42 +18,51 @@ import snowflake.connector
 from snowflake.connector import connect
 snowflake_pull = connect(user='Dave', password='Quantum314!', account='fva14998.us-east-1')
 
+
+test = gc.open_by_key('1fSUztuAQ_mizi8cSHQCIRUuNwbttWBdHWDItXLtosR4')
+
+
+
+
 ##Import delayed processing RIs
-sql_delayed_ris = ("""
+sql_ris = ("""
 select
   reimbursement_invoice_products.product_condition_id as "pcid"
   , reimbursement_invoice_products.expected_quantity as "expected_quantity"
   , reimbursement_invoices.received_at_et::date as "receipt_date"
-  , reimbursement_invoices.processing_ended_at_et::date as "processing_date"
+  //, reimbursement_invoices.processing_ended_at_et::date as "processing_date"
+  , reimbursement_invoices.shelved_at_et as "shelving_timestamp"
   , reimbursement_invoices.reimbursement_invoice_number as "ri_number"
 
 from analytics.core.reimbursement_invoices
     inner join analytics.core.reimbursement_invoice_products on reimbursement_invoices.reimbursement_invoice_number = reimbursement_invoice_products.reimbursement_invoice_number
 
 where
-  reimbursement_invoices.received_at_et::date >= cast(dateadd(dd, -90, getdate()) as date)
+  //reimbursement_invoices.received_at_et::date >= cast(dateadd(dd, -5, getdate()) as date)
+  reimbursement_invoices.received_at_et::date >='2024-12-29'
   and reimbursement_invoices.was_marked_missing = False
-  and datediff(dd, reimbursement_invoices.received_at_et, reimbursement_invoices.processing_ended_at_et) > 1
-  and datediff(dd, reimbursement_invoices.received_at_et, reimbursement_invoices.processing_ended_at_et) < 30
+  and datediff(dd, reimbursement_invoices.received_at_et, reimbursement_invoices.shelved_at_et) > 1
+  and datediff(dd, reimbursement_invoices.received_at_et, reimbursement_invoices.shelved_at_et) < 30
   and "receipt_date" is not null
-  and "processing_date" is not null
+  and "shelving_timestamp" is not null
+  //and "processing_date" is not null
 
 order by 1, 3, 4
 """)
 
 cursor = snowflake_pull.cursor()
-cursor.execute(sql_delayed_ris)
+cursor.execute(sql_ris)
 
-delayed_ris_df = cursor.fetch_pandas_all()
-delayed_ris_df.drop(delayed_ris_df.filter(like='Unnamed'), axis=1, inplace=True)
-delayed_ris_df.dropna(subset = ['pcid'], inplace=True)
+ris_df = cursor.fetch_pandas_all()
+ris_df.drop(ris_df.filter(like='Unnamed'), axis=1, inplace=True)
+ris_df.dropna(subset = ['pcid'], inplace=True)
 
 ##Import non-direct order information
 order_sql = ("""
 select
   order_items.product_condition_id as "pcid"
   , order_items.unit_price_usd as "sold_price"
-  , order_items.quantity as "sold_qty"
+  , order_items.quantity as "sold_qty_per_seller_order"
   , order_items.ordered_at_et::date as "ordered_at_date"
   , order_items.ordered_at_et as "ordered_at_timestamp"
   , seller_orders.order_number as "seller_order_number"
@@ -63,8 +72,11 @@ from analytics.core.order_items
   left outer join analytics.core.sellers on seller_orders.seller_id = sellers.id
 
 where
-  order_items.ordered_at_et::date >= cast(dateadd(dd, -90, getdate()) as date)
+  order_items.ordered_at_et::date >= cast(dateadd(dd, -46, getdate()) as date)
+  and order_items.ordered_at_et::date <= cast(dateadd(dd, -0, getdate()) as date)
+  //order_items.ordered_at_et::date = '2025-01-05'
   and seller_orders.is_direct_order = False
+  //and "seller_order_number" = '4AEE8829-37A062-43C18'
 
 order by
     "ordered_at_timestamp" asc
@@ -83,119 +95,207 @@ marketplace_value_per_day = sales_per_day_df.groupby('ordered_at_date')['sold_pr
 sales_per_day_df = pd.merge(sales_per_day_df, marketplace_value_per_day, how='right', on='ordered_at_date')
 sales_per_day_df.rename(columns={'sold_price_x':'sold_price', 'sold_price_y':'total_sold_price_per_day'}, inplace=True)
 
-marketplace_qty_per_day = sales_per_day_df.groupby('ordered_at_date')['sold_qty'].sum()
+marketplace_qty_per_day = sales_per_day_df.groupby('ordered_at_date')['sold_qty_per_seller_order'].sum()
 sales_per_day_df = pd.merge(sales_per_day_df, marketplace_qty_per_day, how='right', on='ordered_at_date')
-sales_per_day_df.rename(columns={'sold_qty_x':'sold_qty', 'sold_qty_y':'total_sold_qty_per_day'}, inplace=True)
+sales_per_day_df.rename(columns={'sold_qty_per_seller_order_x':'sold_qty_per_seller_order', 'sold_qty_per_seller_order_y':'total_sold_qty_per_day'}, inplace=True)
 
-sales_per_day_df.drop_duplicates(subset='ordered_at_date', inplace=True)
+##Merge frames
+ris_df = pd.merge(ris_df, order_df, how='left', on='pcid')
+ris_df.dropna(subset='ordered_at_date', inplace=True)
 
-##Find orders for pcids that happened during processing delay
-delayed_ris_df = pd.merge(delayed_ris_df, order_df, how='left', on='pcid')
+##Exclude Ris not in backlog when order placed
+ris_df["exclude"] = 0
 
-delayed_ris_df = delayed_ris_df.loc[(delayed_ris_df['receipt_date'] < delayed_ris_df['ordered_at_date']) & (delayed_ris_df['processing_date'] > delayed_ris_df['ordered_at_date'])]
+#ris_df.loc[(ris_df['receipt_date'] < ris_df['ordered_at_date']) & (ris_df['processing_date'] > ris_df['ordered_at_date']), 'exclude'] = 1
+ris_df.loc[(ris_df['receipt_date'] < ris_df['ordered_at_date']) & (ris_df['shelving_timestamp'] > ris_df['ordered_at_timestamp']), 'exclude'] = 1
 
-delayed_ris_df["pcid_identifier"] = delayed_ris_df['pcid'].astype(str) + '~' + delayed_ris_df['ri_number'].astype(str)
+max_exclude = ris_df.groupby('seller_order_number')['exclude'].min()
+ris_df = pd.merge(ris_df, max_exclude, how='right', on='seller_order_number')
+ris_df.rename(columns={'exclude_x':'exclude', 'exclude_y':'max_exclude'}, inplace=True)
 
-delayed_ris_df["combined"] = delayed_ris_df['pcid'].astype(str) + '~' + delayed_ris_df['ordered_at_date'].astype(str)
+ris_df = ris_df.loc[ris_df['max_exclude'] == 1]
 
-delayed_ris_df.rename(columns={'sold_qty':'sold_qty_per_seller_order'}, inplace=True)
+ris_df.drop(['exclude', 'max_exclude'], axis=1, inplace=True)
 
-##Loop frame contents until all sales are accounted for
-def process_data(min_length = 0):
-    dataframe = delayed_ris_df
-    orders_df = pd.DataFrame()
+ris_df.sort_values(by=['ordered_at_timestamp', 'pcid', 'expected_quantity'], ascending=[True, True, False], inplace=True)
 
-    ##Establish variables
-    dataframe["keep_order"] = 0.0
-    dataframe["missed_sales_qty"] = 0.0
-    dataframe["remaining_to_fill"] = 0.0
-    dataframe["remaining_to_sell"] = 0.0
-    dataframe['remaining_to_sell'] = dataframe['expected_quantity']
+##Exclude orders we could never fulfill with waiting product
+ris_df["pcid_per_day"] = ris_df['pcid'].astype(str) + '~' + ris_df['ordered_at_date'].astype(str)
 
-    while len(dataframe) > min_length:
-        print("Orders to Evaluate Frame Length: " + str(len(dataframe)))
-        print("Missed Orders Frame Length: " +  str(len(orders_df)))
-        print(len(dataframe) + len(orders_df))
+expected_qty_per_day = ris_df.groupby('pcid_per_day')['expected_quantity'].sum()
+ris_df = pd.merge(ris_df, expected_qty_per_day, how='right', on='pcid_per_day')
+ris_df.rename(columns={'expected_quantity_x':'expected_quantity', 'expected_quantity_y':'total_pcid_per_day'}, inplace=True)
 
-        ##Find min order timestamp per pcid/order date
-        min_order_timestamp = dataframe.groupby('pcid_identifier')['ordered_at_timestamp'].min()
-        dataframe = pd.merge(dataframe, min_order_timestamp, how='right', on='pcid_identifier')
-        dataframe.rename(columns={'ordered_at_timestamp_x':'ordered_at_timestamp', 'ordered_at_timestamp_y':'min_ordered_at_timestamp'}, inplace=True)
+ris_df.drop(['pcid_per_day'], axis=1, inplace=True)
+ris_df.dropna(subset='seller_order_number', inplace=True)
 
-        dataframe.loc[dataframe['ordered_at_timestamp'].astype(str) == dataframe['min_ordered_at_timestamp'].astype(str), 'keep_order'] = 1
+ris_df["pcid_id"] = ris_df['pcid'].astype(str) + '~' + ris_df['ri_number'].astype(str)
+ris_df["pcid_seller_order_id"] = ris_df['pcid'].astype(str) + '~' + ris_df['seller_order_number'].astype(str)
 
-        dataframe['remaining_to_sell'] = dataframe['remaining_to_sell'].astype('float64') - dataframe['sold_qty_per_seller_order'].astype('float64')
+ris_df["exclude"] = ris_df['total_pcid_per_day'] - ris_df['sold_qty_per_seller_order']
 
-        dataframe.loc[dataframe['remaining_to_sell'] < 0, 'missed_sales_qty'] = dataframe['expected_quantity']
-        dataframe.loc[dataframe['remaining_to_sell'] >= 0, 'missed_sales_qty'] = dataframe['sold_qty_per_seller_order']
+min_exclude = ris_df.groupby('seller_order_number')['exclude'].min()
+ris_df = pd.merge(ris_df, min_exclude, how='right', on='seller_order_number')
+ris_df.rename(columns={'exclude_x':'exclude', 'exclude_y':'min_exclude'}, inplace=True)
 
-        dataframe.loc[dataframe['remaining_to_sell'] < 0, 'remaining_to_sell'] = 0.0
+ris_df = ris_df.loc[ris_df['min_exclude'] >= 0]
 
-        dataframe = dataframe.loc[dataframe['remaining_to_sell'] > 0]
+ris_df["pcid_id"] = ris_df['pcid'].astype(str) + '~' + ris_df['ri_number'].astype(str)
+ris_df["pcid_seller_order_id"] = ris_df['pcid'].astype(str) + '~' + ris_df['seller_order_number'].astype(str)
 
-        ##Split orders
-        orders_to_fulfill = dataframe.copy()
-        orders_to_fulfill = orders_to_fulfill.loc[orders_to_fulfill['keep_order'] == 1]
+ris_df.drop(['total_pcid_per_day', 'exclude', 'min_exclude'], axis=1, inplace=True)
 
-        orders_to_evaluate = dataframe.copy()
-        orders_to_evaluate = orders_to_evaluate.loc[orders_to_evaluate['keep_order'] == 0]
+ris_df["remaining_to_sell"] = 0.0
+ris_df["remaining_to_fill"] = 0.0
+ris_df["order_part_complete"] = "No"
+ris_df["pcid_check"] = ris_df['pcid_id']
+ris_df["seller_check"] = ris_df['pcid_seller_order_id']
 
-        ##Fulfill orders
-        orders_to_fulfill['remaining_to_fill'] = orders_to_fulfill['sold_qty_per_seller_order'].astype('float64') - orders_to_fulfill['missed_sales_qty'].astype('float64')
+filled_order_parts = pd.DataFrame()
 
-        orders_df = pd.concat([orders_df, orders_to_fulfill])
+##Create list of unique orders to try and fulfill then loop through them
+unique_orders_df = ris_df.copy()
+unique_orders_df.drop_duplicates(subset='seller_order_number', inplace=True)
+unique_orders_df = unique_orders_df[['seller_order_number']]
 
-        dataframe = orders_to_evaluate
+print(len(unique_orders_df));
 
-        dataframe.drop('min_ordered_at_timestamp', axis=1, inplace=True)
-        orders_df.drop('min_ordered_at_timestamp', axis=1, inplace=True)
+s = len(unique_orders_df);
+#s = 1000;
+i = 0;
 
-    return orders_df, dataframe
+while i < s:
+    order_to_fill = unique_orders_df.head(1)
 
-orders_df, dataframe = process_data()
+    int_check = divmod(i + 1, 100)##########################
+
+    if int_check[1] == 0:
+        print(i + 1)
+        executionTime = (time.time() - startTime)
+        print(executionTime)
+
+    order_to_fill = pd.merge(order_to_fill, ris_df, how='left', on='seller_order_number')
+
+    l = len(order_to_fill);
+    j = 0;
+
+    while j < l:
+        order_part_to_fill = order_to_fill.head(1)
+
+        order_part_to_fill.iloc[:,12] = order_part_to_fill['expected_quantity'] - order_part_to_fill['sold_qty_per_seller_order'] #remaining_to_sell
+        order_part_to_fill.loc[order_part_to_fill['remaining_to_sell'] < 0, 'remaining_to_sell'] = 0
+
+        order_part_to_fill.iloc[:,13] = order_part_to_fill['sold_qty_per_seller_order'] - order_part_to_fill['expected_quantity'] #remaining_to_fill
+        order_part_to_fill.loc[order_part_to_fill['remaining_to_fill'] < 0, 'remaining_to_fill'] = 0
+
+        order_part_to_fill.loc[order_part_to_fill['remaining_to_fill'] == 0, 'order_part_complete'] = "Yes"
+
+        #Merge completed order parts with filled_order_parts dataframe
+        order_part_to_fill_complete = order_part_to_fill.copy()
+        order_part_to_fill_complete = order_part_to_fill_complete.loc[order_part_to_fill_complete['order_part_complete'] == "Yes"]
+
+        if len(filled_order_parts) > 0:
+            filled_order_parts = pd.concat([filled_order_parts, order_part_to_fill_complete])
+
+        if len(filled_order_parts) == 0:
+            filled_order_parts = order_part_to_fill_complete
+
+        #Merge to order_to_fill frame and adjust exp_qty and remove fulfilled order parts
+        order_to_fill = pd.merge(order_to_fill, order_part_to_fill, how='left', on='pcid_id')
+
+        order_to_fill.loc[order_to_fill['pcid_check_y'] == order_to_fill['pcid_check_x'], 'expected_quantity_x'] = order_to_fill['remaining_to_sell_y']
+
+        order_to_fill.drop(list(order_to_fill.filter(regex="_y")), axis=1, inplace=True)
+
+        char_to_find = "_x"
+        new_columns = [col.replace(char_to_find, "")if char_to_find in col else col for col in order_to_fill.columns]
+        order_to_fill.columns = new_columns
+
+        order_to_fill = pd.merge(order_to_fill, order_part_to_fill, how='left', on='pcid_seller_order_id')
+
+        order_to_fill.loc[order_to_fill['seller_check_y'] == order_to_fill['seller_check_x'], 'order_part_complete_x'] = order_to_fill['order_part_complete_y']
+
+        order_to_fill.drop(list(order_to_fill.filter(regex="_y")), axis=1, inplace=True)
+
+        char_to_find = "_x"
+        new_columns = [col.replace(char_to_find, "")if char_to_find in col else col for col in order_to_fill.columns]
+        order_to_fill.columns = new_columns
+
+        order_to_fill = order_to_fill.loc[(order_to_fill['expected_quantity'] > 0) & (order_to_fill['order_part_complete'] == "No")]
+
+
+        #Merge examined order back with original dataframe to adjust expected_quantity and remove fulfilled order parts
+        ris_df = pd.merge(ris_df, order_part_to_fill, how='left', on='pcid_id')
+
+        ris_df.loc[ris_df['pcid_check_y'] == ris_df['pcid_check_x'], 'expected_quantity_x'] = ris_df['remaining_to_sell_y']
+
+        ris_df.drop(list(ris_df.filter(regex="_y")), axis=1, inplace=True)
+
+        char_to_find = "_x"
+        new_columns = [col.replace(char_to_find, "")if char_to_find in col else col for col in ris_df.columns]
+        ris_df.columns = new_columns
+
+
+        ris_df = pd.merge(ris_df, order_part_to_fill, how='left', on='pcid_seller_order_id')
+
+        ris_df.loc[ris_df['seller_check_y'] == ris_df['seller_check_x'], 'order_part_complete_x'] = ris_df['order_part_complete_y']
+
+        ris_df.drop(list(ris_df.filter(regex="_y")), axis=1, inplace=True)
+
+        char_to_find = "_x"
+        new_columns = [col.replace(char_to_find, "")if char_to_find in col else col for col in ris_df.columns]
+        ris_df.columns = new_columns
+
+        ris_df = ris_df.loc[(ris_df['expected_quantity'] > 0) & (ris_df['order_part_complete'] == "No")]
+
+        j+=1
+
+    unique_orders_df = unique_orders_df.iloc[1:]
+
+    i+=1
+
+
+filled_order_parts.drop(['order_part_complete', 'pcid_check', 'seller_check', 'remaining_to_fill', 'pcid_id', 'pcid_seller_order_id'], axis=1, inplace=True)
+
+
+filled_order_parts.sort_values(by=['ordered_at_timestamp', 'seller_order_number'], ascending=[True, True], inplace=True)
+
+
+
+
+
+filled_order_parts_tab = test.worksheet('filled_order_parts')
+filled_order_parts_tab.clear()
+gd.set_with_dataframe(filled_order_parts_tab, filled_order_parts)##############################
 
 ##Aggragate values per day
-orders_df["monetary_loss"] = orders_df['missed_sales_qty'].astype('float64') * orders_df['sold_price'].astype('float64')
+filled_order_parts["monetary_loss"] = filled_order_parts['sold_qty_per_seller_order'].astype('float64') * filled_order_parts['sold_price'].astype('float64')
 
-missed_value_per_day = orders_df.groupby('ordered_at_date')['monetary_loss'].sum()
-orders_df = pd.merge(orders_df, missed_value_per_day, how='right', on='ordered_at_date')
-orders_df.rename(columns={'monetary_loss_x':'monetary_loss', 'monetary_loss_y':'monetary_loss_per_day'}, inplace=True)
+missed_value_per_day = filled_order_parts.groupby('ordered_at_date')['monetary_loss'].sum()
+filled_order_parts = pd.merge(filled_order_parts, missed_value_per_day, how='right', on='ordered_at_date')
+filled_order_parts.rename(columns={'monetary_loss_x':'monetary_loss', 'monetary_loss_y':'monetary_loss_per_day'}, inplace=True)
 
-missed_sales_per_day = orders_df.groupby('ordered_at_date')['missed_sales_qty'].sum()
-orders_df = pd.merge(orders_df, missed_sales_per_day, how='right', on='ordered_at_date')
-orders_df.rename(columns={'missed_sales_qty_x':'missed_sales_qty', 'missed_sales_qty_y':'missed_sales_per_day'}, inplace=True)
+missed_sales_per_day = filled_order_parts.groupby('ordered_at_date')['sold_qty_per_seller_order'].sum()
+filled_order_parts = pd.merge(filled_order_parts, missed_sales_per_day, how='right', on='ordered_at_date')
+filled_order_parts.rename(columns={'sold_qty_per_seller_order_x':'sold_qty_per_seller_order', 'sold_qty_per_seller_order_y':'missed_sales_per_day'}, inplace=True)
 
-orders_df.drop_duplicates(subset='ordered_at_date', inplace=True)
-orders_df = orders_df[['ordered_at_date', 'monetary_loss_per_day', 'missed_sales_per_day']]
+filled_order_parts.drop_duplicates(subset='ordered_at_date', inplace=True)
+filled_order_parts = filled_order_parts[['ordered_at_date', 'monetary_loss_per_day', 'missed_sales_per_day']]
 
 ##Combine frames
-orders_df = pd.merge(orders_df, sales_per_day_df, how='left', on='ordered_at_date')
+filled_order_parts = pd.merge(filled_order_parts, sales_per_day_df, how='left', on='ordered_at_date')
 
-orders_df["monetary_loss_%"] = orders_df['monetary_loss_per_day'].astype('float64') / orders_df['total_sold_price_per_day'].astype('float64')
+filled_order_parts["monetary_loss_%"] = filled_order_parts['monetary_loss_per_day'].astype('float64') / filled_order_parts['total_sold_price_per_day'].astype('float64')
 
-orders_df["missed_sales_%"] = orders_df['missed_sales_per_day'].astype('float64') / orders_df['total_sold_qty_per_day'].astype('float64')
+filled_order_parts["missed_sales_%"] = filled_order_parts['missed_sales_per_day'].astype('float64') / filled_order_parts['total_sold_qty_per_day'].astype('float64')
 
-orders_df = orders_df[['ordered_at_date', 'monetary_loss_per_day', 'missed_sales_per_day', 'monetary_loss_%', 'missed_sales_%']]
-
-
+filled_order_parts = filled_order_parts[['ordered_at_date', 'monetary_loss_per_day', 'missed_sales_per_day', 'monetary_loss_%', 'missed_sales_%']]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+filled_order_parts.drop_duplicates(subset='ordered_at_date', inplace=True)
 
 ##Write data to sheet
-dataTab = gc.open_by_key('1_w6u8R5TACFRcsLLbCHOyOTNwSSmL08eyqU00SDwCrw').worksheet('Data')
-dataTab.batch_clear(['A2:E'])
-gd.set_with_dataframe(dataTab, orders_df, row=2, col=1)
+filled_order_parts_tab = test.worksheet('Output')
+filled_order_parts_tab.clear()
+gd.set_with_dataframe(filled_order_parts_tab, filled_order_parts)##############################
+
